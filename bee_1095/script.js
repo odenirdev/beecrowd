88e9367e202d7ec739fd8b2a508cb1268ed01aa4
Odenir Gomes <odenirdev@gmail.com>
2024-12-15 16:55:25 -0300

    feat: resolved bee 1095

--- bee_1095/script.js
+++ bee_1095/script.js
@@ -1,4 +1,11 @@
 var input = require("fs").readFileSync("./stdin", "utf8");
 var lines = input.split("\n");
 
-for (let i = 0; i < lines.length; i++) {}
+let i = 1;
+let j = 60;
+
+while (j >= 0) {
+  console.log(`I=${i} J=${j}`);
+  j -= 5;
+  i += 3;
+}
