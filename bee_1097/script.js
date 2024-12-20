8b5b670afe440b9dcc36fd9a7818176e715a5bfb
Odenir Gomes <odenirdev@gmail.com>
2024-12-20 08:06:27 -0300

    resolved 1097

--- bee_1097/script.js
+++ bee_1097/script.js
@@ -1,8 +1,16 @@
 var input = require("fs").readFileSync("./stdin", "utf8");
 var lines = input.split("\n");
 
-for (let i = 1; i <= 9; i += 2) {
-  for (let j = i + 6; j >= i + 4; j--) {
-    console.log(`I=${i} J=${j}`);
+for (let i = 0; i <= 2; i = Math.round((i + 0.2) * 10) / 10) {
+  for (let j = 1; j <= 3; j++) {
+    const displayI = Math.round(i) === i ? i.toFixed(0) : i.toFixed(1);
+
+    let displayJ = j + i;
+    displayJ =
+      Math.round(displayJ) === displayJ
+        ? displayJ.toFixed(0)
+        : displayJ.toFixed(1);
+
+    console.log(`I=${displayI} J=${displayJ}`);
   }
 }
